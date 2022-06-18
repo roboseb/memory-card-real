33609eb49a252075d45c5f6082e6abb73d307230
roboseb <sebastienapril@rocketmail.com>
2022-06-17 19:41:49 -0600

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+
+async function getCards() {
+    let response;
+    try {
+        response = await fetch(`https://api.magicthegathering.io/v1/cards?set=MMA&page=3`);
+    } catch (err) {
+        alert(err);
+    }
+
+    const cardData = await response.json();
+
+    console.log(cardData)
+}
+
+getCards();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +28,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
